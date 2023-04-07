a4d71d3206a1b4a97652da751673f91ed7931adb
teo <theo.parashkevov@gmail.com>
2023-04-07 11:19:27 +0300

    Answer 57

---
+++ Python/57/main.py
@@ -0,0 +1,16 @@
+
+p = 1
+q = 1
+counter = 0
+
+# 1000 iterations
+for i in range(1000):
+    a1 = p + 2*q
+    b1 = p + q
+    if len(str(a1)) > len(str(b1)):
+        counter += 1
+    p = a1
+    q = b1
+
+# printing the counter
+print(counter)
