d7f85df89caac0a4b4636178143001ea0bedfd43
theoparashkevov <theo.parashkevov@gmail.com>
2023-04-11 12:30:09 +0300

    Answer 62

---
+++ Python/62/main.py
@@ -0,0 +1,13 @@
+from collections import defaultdict
+def cube(x): return x**3
+
+def main():
+    cubes = defaultdict(list)
+    for i in range(10000):
+        c = cube(i)
+        digits = ''.join(sorted([d for d in str(c)]))
+        cubes[digits].append(c)
+    print(min([min(v) for k, v in list(cubes.items()) if len(v) == 5]))
+
+if __name__ == "__main__":
+    main()
